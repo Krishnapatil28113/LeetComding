6b3fee2d8b6791e80823e3d0dda79974671065a1
krishna <kp1406055@gmail.com>
2024-07-13 08:27:18 +0530

    Water Bottles

--- water_bottles.cpp
+++ water_bottles.cpp
@@ -8,7 +8,7 @@
     --> After drinking all the 15 bottles, (15/4) filled bottles can be generated, i.e, 3
     --> Now, as 3 < numExchange, we won't be able to group them and generate a water bottle
     --> So, we will drink these 3 bottles and then we will be having, (3(15/4) + 3(15%4)) empty bottles
-    --> Now, we have 6 empty bottles, therefore, (6/4), i.e, we can 1 group
+    --> Now, we have 6 empty bottles, therefore, (6/4), i.e, we can have 1 group
 * Therefore, the basic idea is to count the number of groups getting formed, adding them, and at each stage, check for the remainder, and then perform the further operations on the sum of both
     --> as they can contribute in generating a group
 ---------------------------------------------------------------
